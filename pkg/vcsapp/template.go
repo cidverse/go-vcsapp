ec11615c5253e512ceff17289583aa191210348c
Philipp Heuer <git@philippheuer.me>
2023-09-03 22:49:40 +0200

    feat: add contains func to go template render func

--- pkg/vcsapp/template.go
+++ pkg/vcsapp/template.go
@@ -6,12 +6,28 @@ import (
 	"text/template"
 )
 
+var templateFuncMap = template.FuncMap{
+	"contains": func(container interface{}, elem interface{}) bool {
+		switch c := container.(type) {
+		case []string:
+			for _, v := range c {
+				if v == elem.(string) {
+					return true
+				}
+			}
+			return false
+		default:
+			return false
+		}
+	},
+}
+
 func Render(tpl string, data interface{}) ([]byte, error) {
 	return RenderWithCustomDelimiter(tpl, "{{", "}}", data)
 }
 
 func RenderWithCustomDelimiter(tpl string, leftDelimiter string, rightDelimiter string, data interface{}) ([]byte, error) {
-	tmpl, err := template.New("template").Delims(leftDelimiter, rightDelimiter).Parse(tpl)
+	tmpl, err := template.New("template").Delims(leftDelimiter, rightDelimiter).Funcs(templateFuncMap).Parse(tpl)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse template: %w", err)
 	}
